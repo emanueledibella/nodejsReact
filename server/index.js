eebfc1be7bacfea2b4c3ca4208be301a64759831
Emanuele Di Bella <55705095+emanueledibella@users.noreply.github.com>
2022-06-28 15:25:32 +0200

    Added mysql

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM customers", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
+
 
 app.get("/", (req, res) => {
   res.end("complimenti"); 
@@ -10,6 +28,16 @@ app.get("/", (req, res) => {
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+app.get("/db", (req, res) => {
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM customers", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
 });
 
 app.listen(PORT, () => {
